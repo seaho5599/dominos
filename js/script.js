e9284f6f285d1e043974432a88b50df9c32b0568
seaho5599 <seaho5599@gmail.com>
2022-07-26 17:19:51 +0900

    클론코딩작업중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+  let gnbMoreBtn = $('.gnb-more-btn');
+  let subMenu = $('.submenu');
+  gnbMoreBtn.click(function(event){
+    event.preventDefault();
+    subMenu.show();
+  })
+});
+
+
+
 window.onload = function(){
   
 }
